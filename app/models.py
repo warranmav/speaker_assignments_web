8da4388332f8f87b0e1c5f411585c56f71ab4b10
warranmav <warran@wozland.co.uk>
2024-08-16 15:58:48 +0100

    Add Manage Assignments menu with links to assignment management actions
    Add Manage Assignments to allow update and delete

--- app/models.py
+++ app/models.py
@@ -5,10 +5,7 @@ class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     exception = db.Column(db.String(20), nullable=True)
-    included_in_pool = db.Column(db.Boolean, default=True)
-    date_last_spoken = db.Column(db.Date, nullable=True)
-    speaker_pos = db.Column(db.String(50), nullable=True)
-    talk_length = db.Column(db.String(20), nullable=True)
+
 
     # Relationship to TalkAssignment
     assignments = db.relationship('TalkAssignment', backref='speaker', lazy=True)
