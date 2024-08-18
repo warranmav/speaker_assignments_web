ad50a923d94334b4d273624e0aff203063737a11
warranmav <warran@wozland.co.uk>
2024-08-18 13:24:44 +0100

    Refactor view assignments to use new table. refactor view assignments to show

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,18 +1,17 @@
 from flask import Blueprint, render_template
 from datetime import datetime
-from app.models import Record
+from app.models import TalkAssignment, Record
 
 bp = Blueprint('main', __name__)
-
 
 @bp.route('/')
 def index():
     today = datetime.today()
     current_year_month = today.strftime('%Y-%m')  # Format: '2024-08'
 
-    records = Record.query.filter(
-        Record.date_last_spoken != None,
-        Record.date_last_spoken.startswith(current_year_month)
-    ).order_by(Record.date_last_spoken).all()
+    # Fetch assignments for the current month
+    assignments = TalkAssignment.query.filter(
+        TalkAssignment.date.startswith(current_year_month)
+    ).order_by(TalkAssignment.date).all()
 
-    return render_template('index.html', assignments=records)
+    return render_template('index.html', assignments=assignments)
--- app/routes/view.py
+++ app/routes/view.py
@@ -1,19 +1,20 @@
 from flask import Blueprint, render_template
-from app.models import Record
+from app.models import TalkAssignment
 from datetime import datetime
 
 bp = Blueprint('view', __name__, url_prefix='/view')
 
 @bp.route('/assignments', methods=['GET'])
 def view_assignments():
-    records = Record.query.filter(Record.date_last_spoken != None).order_by(Record.date_last_spoken).all()
+    # Query TalkAssignment instead of Record
+    assignments_query = TalkAssignment.query.filter(TalkAssignment.date != None).order_by(TalkAssignment.date).all()
 
     assignments = {}
-    for record in records:
-        date = record.date_last_spoken
+    for assignment in assignments_query:
+        date = assignment.date
         year_month = date.strftime('%Y %B')  # Format: '2024 August'
         if year_month not in assignments:
             assignments[year_month] = []
-        assignments[year_month].append(record)
+        assignments[year_month].append(assignment)
 
     return render_template('view_assignments.html', assignments=assignments)
